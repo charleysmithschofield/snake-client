505f642ca2dd5f7141697b50e5655359d9f28ca6
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-22 23:00:53 -0700

    added an if statement to check if the user typed CTRL + C, and if they did, exit the program

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 // input.js
-let connection; // This will hold the connection object
+// This will hold the connection object
+let connection;
 
+// sets up the user input
 const setupInput = function(conn) {
   connection = conn; // Store the connection object passed to setupInput
   const stdin = process.stdin;
@@ -12,17 +14,27 @@ const setupInput = function(conn) {
   return stdin;
 };
 
+// define handleUserInput function
 const handleUserInput = function(data) {
-  if (data === 'w' || data === "W") {
+  // Check for CTRL + C
+  if (data === '\u0003') {
+    // If found, exit the process
+    process.exit(0);
+    // If user types w or W move the snake up
+  } else if (data === 'w' || data === "W") {
     connection.write("Move: up");
+    // if user types a or A move the snake left
   } else if (data === 'a' || data === "A") {
     connection.write("Move: left");
+    // if the user types s or S move the snake down
   } else if (data === 's' || data === "S") {
     connection.write("Move: down");
+    // if the user types d or D move the snake right
   } else if (data === 'd' || data === "D") {
     connection.write("Move: right");
   }
 };
 
+// exports the setupInput file
 module.exports = { setupInput };
 
