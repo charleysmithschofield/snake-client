bbc16d46c4cbaf2628442e760fd6c9596b7b80ac
charleysmithschofield <charleysmithschofield@gmail.com>
2024-02-22 22:12:50 -0700

    separated the inputuser functions into their own file called input.js

--- client.js
+++ client.js
@@ -1,52 +1,28 @@
+// client.js
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "localhost", // IP address here,
     port: 50541 // PORT number here,
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  // handle incoming data and log it for the player
   conn.on("data", (data) => {
-    console.log(`"Received data", ${data}`);
+    console.log(`Received data: ${data}`);
   });
 
-  // handle connection established
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-    // set the name
     const name = process.argv[2];
-
-    // Send the name of each snake as soon as the connection is established
-    conn.write(`"Name: ${name}"`);
+    conn.write(`Name: ${name}`);
   });
 
-  // function to set up userInput
-  const setupInput = function () {
-    const stdin = process.stdin;
-    stdin.setRawMode(true);
-    stdin.setEncoding("utf8");
-    stdin.resume();
-    stdin.on("data", handleUserInput);
-
-    // Function to handle user input
-    const handleUserInput = function (data) {
-      if (data === 'w' || data === "W") {
-        conn.write("Move: up");
-      }
-    };
-    return stdin;
-  };
-  
   return conn;
 };
 
 console.log("Connecting ...");
 
-module.exports = connect;
-
-
+module.exports = { connect };
